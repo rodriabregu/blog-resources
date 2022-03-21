75f3c2c64923c609269e83fbb45fd29edbba2398
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-03-21 00:03:33 -0300

    project-config :pencil2:

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,19 +5,19 @@ const categories = [{name: 'React', slug: 'react'}, {name: 'Web Developer', slug
 
 const Header = () => {
   return (
-    <div className='container mx-auto px-10 mb-8'>
-      <div className='border-b w-full inline-block border-blue-400 py-8'>
-        <div className='md:float-left block'>
+    <div className='container px-10 mx-auto mb-8'>
+      <div className='inline-block w-full py-8 border-b border-blue-400'>
+        <div className='block md:float-left'>
           <Link href='/'>
-            <span className='cursor-pointer font-bold text-4xl text-white'>
-              GraphCMS
+            <span className='text-4xl font-bold text-white cursor-pointer'>
+              Blog Resources
             </span>
           </Link>
         </div>
         <div className='hidden md:float-left md:contents'>
           {categories.map(category => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+              <span className='mt-2 ml-4 font-semibold text-white align-middle cursor-pointer md:float-right'>
                 {category.name}
               </span>
             </Link>
