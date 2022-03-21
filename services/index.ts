ef98a86fc52c9a44f8596688508b238ce9f10255
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-03-21 00:00:30 -0300

    service/recent-similar-posts :postbox:

--- services/index.ts
+++ services/index.ts
@@ -31,3 +31,43 @@ export const getPosts = async () => {
   const results = await request(graphqlAPI, query)
   return results.postsConnection.edges
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        coverImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+    const results = await request(graphqlAPI, query)
+    return results.posts
+}
+
+export const getSimilarPosts = async (categories: any, slug: any) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: { slug_not: $slug, AND: {categories_some: { slug_in: $categories } } }
+        last: 3
+      ) {
+        title
+        coverImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+    const results = await request(graphqlAPI, query, {slug, categories})
+    return results.posts
+}
